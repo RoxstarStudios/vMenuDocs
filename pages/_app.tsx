b200097afb8f5486ec05bcec28732784114c9f6b
XdGoldenTiger <xdgoldentigerbusiness@gmail.com>
2024-09-29 03:15:08 +1300

    Add fix for `/vMenuDocs` redirect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
+
 import './styles.css';
 
 import type { AppProps } from 'next/app'
  
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Check if the current hostname is your Nginx domain
+    if (typeof window !== 'undefined' && window.location.hostname === 'vdocs.roxstarstudios.dev') {
+        const links = document.querySelectorAll('a[href^="/vMenuDocs/"]');
+        links.forEach(link => {
+            link.addEventListener('click', function(event) {
+                event.preventDefault(); // Prevent the default link behavior
+                const newPath = this.getAttribute('href').replace('/vMenuDocs', '');
+                window.location.href = newPath; // Redirect to the new path
+            });
+        });
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
